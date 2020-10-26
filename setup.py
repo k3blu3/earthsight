c049915e365ef4458b17db2aa1629f0667c5aa0d
Krishna Karra <krishna.karra@gmail.com>
2020-10-26 08:52:24 -0600

    python package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='earthsight',
+    version='0.0.1',
+    description='a tool to explore, analyze and understand earth observation data',
+    url='https://github.com/k3blu3/earthsight',
+    author='Krishna Karra',
+    author_email='krishna.karra@gmail.com',
+    license='Creative Commons',
+    packages=['earthsight'],
+    setup_requires=['setuptools>50'],
+    install_requires=[]
+)
